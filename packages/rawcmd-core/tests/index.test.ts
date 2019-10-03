0aa87064cabdd4dc59642efb0a68c53f48e5f16e
Kristijan Sedlak <xpepermint@gmail.com>
2019-10-03 23:02:05 +0200

    Fix tests

--- packages/rawcmd-core/tests/index.test.ts
+++ packages/rawcmd-core/tests/index.test.ts
@@ -4,14 +4,16 @@ import * as exp from '../src';
 const spec = new Spec();
 
 spec.test('exposed content', (ctx) => {
-  ctx.true(!!exp.Typewriter);
-  ctx.true(!!exp.Spinner);
-  ctx.true(!!exp.ConsoleStreamlet);
-  ctx.true(!!exp.MemoryStreamlet);
   ctx.true(!!exp.Command);
+  ctx.true(!!exp.ConsoleStreamlet);
+  ctx.true(!!exp.EOL);
   ctx.true(!!exp.ErrorCode);
   ctx.true(!!exp.GenericError);
+  ctx.true(!!exp.MemoryStreamlet);
+  ctx.true(!!exp.Option);
   ctx.true(!!exp.RuntimeError);
+  ctx.true(!!exp.Spinner);
+  ctx.true(!!exp.Typewriter);
   ctx.true(!!exp.ValidationError);
 });
 
